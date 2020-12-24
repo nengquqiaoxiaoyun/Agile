59e5679b5989cae29efda7d8d934f866dd8ed8fc
huakaimay <huakaimay@gmail.com>
2020-12-24 09:56:22 +0800

    dip案例

---
+++ src/main/java/cn/cnhoyun/agile/dip/ButtonServer.java
@@ -0,0 +1,11 @@
+package cn.cnhoyun.agile.dip;
+
+/**
+ * 任何知道如何操作此接口的对象都可以控制该接口的实现类
+ * @author: huakaimay
+ * @since: 2020-12-24
+ */
+public interface ButtonServer {
+    void turnOn();
+    void turnOff();
+}
---
+++ src/main/java/cn/cnhoyun/agile/dip/Lamp.java
@@ -0,0 +1,15 @@
+package cn.cnhoyun.agile.dip;
+
+/**
+ * @author: huakaimay
+ * @since: 2020-12-24
+ */
+public class Lamp implements ButtonServer{
+    public void turnOn() {
+        System.out.println(this.getClass().getSimpleName() + " is turn on");
+    }
+
+    public void turnOff() {
+        System.out.println(this.getClass().getSimpleName() + " is turn off");
+    }
+}
